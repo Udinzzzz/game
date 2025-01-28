3566dee0622ce25b368dd64d774cf8ca3d413648
udinzzzz <udin.03zx@gmail.com>
2025-01-28 21:02:46 +0700

    add (wasd) controll

--- input.js
+++ input.js
@@ -1,28 +1,40 @@
-export class InputHandler{
-    constructor(game){
+export class InputHandler {
+    constructor(game) {
         this.keys = []
         this.game = game
-        window.addEventListener('keydown', e =>{
-            if((e.key === 'ArrowDown' ||
+        window.addEventListener('keydown', e => {
+            if ((e.key === 'ArrowDown' ||
                 e.key === 'ArrowUp' ||
                 e.key === 'ArrowRight' ||
                 e.key === 'ArrowLeft' ||
                 e.key === 'Enter')
-                && this.keys.indexOf(e.key) === -1){
+                && this.keys.indexOf(e.key) === -1) {
                 this.keys.push(e.key)
             }
-            else if(e.key === 'd') this.game.debug = !this.game.debug
+            else if (e.key === 's' && this.keys.indexOf(e.key) === -1) this.keys.push('ArrowDown')
+            else if (e.key === 'w' && this.keys.indexOf(e.key) === -1) this.keys.push('ArrowUp')
+            else if (e.key === 'd' && this.keys.indexOf(e.key) === -1) this.keys.push('ArrowRight')
+            else if (e.key === 'a' && this.keys.indexOf(e.key) === -1) this.keys.push('ArrowLeft')
+            else if (e.key === 'e') this.game.debug = !this.game.debug
+
+            console.log(this.keys, "keyDown")
         })
-    
-        window.addEventListener('keyup', e =>{
-            if(e.key === 'ArrowDown' ||
+
+        window.addEventListener('keyup', e => {
+            if (e.key === 'ArrowDown' ||
                 e.key === 'ArrowUp' ||
                 e.key === 'ArrowRight' ||
                 e.key === 'ArrowLeft' ||
                 e.key === 'Enter'
-            ){
+            ) {
                 this.keys.splice(e.key.indexOf(e.key), 1)
             }
+            if (e.key === 's') this.keys.splice(e.key.indexOf('ArrowDown'), 1)
+            if (e.key === 'w') this.keys.splice(e.key.indexOf('ArrowUp'), 1)
+            if (e.key === 'd') this.keys.splice(e.key.indexOf('ArrowRight'), 1)
+            if (e.key === 'a') this.keys.splice(e.key.indexOf('ArrowLeft'), 1)
+
+            console.log(this.keys, "keyUP")
         })
     }
 }
